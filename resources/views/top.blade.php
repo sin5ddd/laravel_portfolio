76d3c712dbebdf63e69c8d8175998d604eb58a76
kitayama <shingo.kitayama@gmail.com>
2025-02-05 15:36:18 +0900

    ログイン関連View修正

---
+++ resources/views/top.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta
+				name="viewport"
+				content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>TOP</title>
+</head>
+<body>
+<div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+	@auth
+		<p><a href="/index">管理ページ</a></p>
+	@else
+		<p><a href="/login">ログイン</a></p>
+	@endauth
+
+</div>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta
+				name="viewport"
+				content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Welcome</title>
+</head>
+<body>
+<div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+	<p><a href="/login">ログイン</a></p>
+</div>
+</body>
+</html>
